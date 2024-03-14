78ef1e0a3d10471b08c2ac6a04cc31a84da87cc4
Hugo Lizarraga <lizarragahugo@gmail.com>
2024-03-14 16:48:55 -0300

    first_commit

---
+++ main/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/FreeRTOSConfig.h"
+#include "driver/gpio.h"
+
+
+void app_main(void)
+{
+    //zero-initialize the config structure.
+    gpio_config_t io_conf = {};
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = 1ULL<<2;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    while(1)
+    {
+        printf("Hola Mundo\r\n");
+        gpio_set_level(2, 0);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        gpio_set_level(2, 1);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        
+    }
+
+}
